7765e1ef903f1c948ed6405a12319028cd17b621
eliasand <elias.andersson2@elev.ga.ntig.se>
2019-09-03 10:55:28 +0200

    python kod försök 1

--- lek1.py
+++ lek1.py
@@ -1,1 +1,12 @@
 print("hej")
+
+firstName = "Elias"
+lastName = 'Andersson'
+
+dateOfBirth = 20010731
+
+age = 18
+teacher = True
+
+print(firstName,lastName,str(age) + str(teacher))
+print(dateOfBirth)
---
+++ lek2.py
@@ -0,0 +1,10 @@
+# variablar för att komma ihåg namn och ålder
+
+# läsa in namn
+name = input("skriv in ditt namn:")
+# läsa in ålder
+age = int(input("skriv in din ålder:"))
+# skriva ut hej namn och du är x år gammal
+
+print("Hejsan",name,",välkommen till mitt program.")
+print("Du är",age,"år gammal")
